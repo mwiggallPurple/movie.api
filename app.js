a8f57081b9c9ae6f2c1ce3a8145375283f2c321a
melissa wiggall <awiggall@aol.co.uk>
2023-01-25 11:22:06 +0000

    api works, cards out of alignment.

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 //const APIlink = 'https://imdb-api.com/en/API/Top250Movies/k_o27z07js';
 
 
-
 getData = () => {
 fetch (APIlink)
 .then ((res) =>res.json())
 .then((data) => {
- refactorData(data.items)
+const reData = refactorData(data.items);
+movieCard(reData);
 });
-}
+};
+
+
 
 refactorData = (movies) => {
     let movieData = []
 
     movies.map ((m) => {
-        movieData.push(
-            {
+        movieData.push({
             id: m.id,
             title: m.title,
             year: m.year,
@@ -24,10 +25,31 @@ refactorData = (movies) => {
             imDbRating: m.imDbRating,
             likes: 0,
             comments: ''
-            }
-        )       
-    })
+            });       
+    });
+return movieData;
+};
 
-    console.log(movieData)
-}
+
+
+movieCard = (reFacMovies) => {
+    let moviesCards = ''
+reFacMovies.map((m)=>{
+moviesCards +=  `<div class="movieCard">
+<img src="${m.image}" alt="${m.title}" />
+<div class="movieCardDetails">
+  <h2>${m.title}</h2>
+  <p>${m.year}</p>
+  <p>IMDB Rating: <span>${m.imDbRating}</span></p>
+  <div class="movieCardSocial">
+    <a href="#"><i class="fas fa-heart"></i></a>
+    <a href="#"><i class="fas fa-share-alt"></i></a>
+    <a href="#"><i class="fas fa-comment"></i></a>
+  </div>
+</div>
+</div>`;
+});
+
+document.querySelector('body').innerHTML = moviesCards
+};
 getData();
